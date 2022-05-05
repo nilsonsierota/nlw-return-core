90c8a09830f849bb7da399a3fa2bd27e8bdc87e7
Billy Everyteen <nilsoncv2@gmail.com>
2022-05-04 22:37:14 -0400

    repositories, use-cases, prisma-feedbacks-repositories, routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
@@ -16,25 +18,28 @@ const transport = nodemailer.createTransport({
 routes.post("/feedback", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
   });
 
-  await transport.sendMail({
-    from: "Team Feedget <oi@feedget.com>",
-    to: "Nilson Sierota <nilsoncv2@gmail.com>",
-    subject: "New Feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: "#111";">`,
-      `<p>Tipo do Feedback: ${type}</p>`,
-      `<p>Comentario: ${comment}</p>`,
-      "</div>",
-    ].join("\n"),
-  });
+  //   await transport.sendMail({
+  //     from: "Team Feedget <oi@feedget.com>",
+  //     to: "Nilson Sierota <nilsoncv2@gmail.com>",
+  //     subject: "New Feedback",
+  //     html: [
+  //       `<div style="font-family: sans-serif; font-size: 16px; color: "#111";">`,
+  //       `<p>Tipo do Feedback: ${type}</p>`,
+  //       `<p>Comentario: ${comment}</p>`,
+  //       "</div>",
+  //     ].join("\n"),
+  //   });
 
-  return res.status(201).json({ data: feedback });
+  return res.status(201).send();
 });
---
+++ src/use-cases/submit-feedback-use-case.ts
@@ -0,0 +1,21 @@
+import { FeedbackRepository } from "../repositories/feedbacks-repository";
+
+interface SubmitFeedbackUseCaseRequest {
+  type: string;
+  comment: string;
+  screenshot?: string;
+}
+
+export class SubmitFeedbackUseCase {
+  constructor(private feedbacksRepository: FeedbackRepository) {}
+
+  async execute(request: SubmitFeedbackUseCaseRequest) {
+    const { type, comment, screenshot } = request;
+
+    await this.feedbacksRepository.create({
+      type,
+      comment,
+      screenshot,
+    });
+  }
+}
