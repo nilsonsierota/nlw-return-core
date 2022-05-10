ad0230f304e80225b72e9bd567f81d515accab95
Billy Everyteen <nilsoncv2@gmail.com>
2022-05-09 20:22:14 -0400

    updates for deploy build

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+
 import { routes } from "./routes";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
