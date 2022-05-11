569f36b3312a9bf76c518f2a0916b32deff7280e
Nilson Sierota <nilsoncv2@gmail.com>
2022-05-11 08:56:04 -0400

    github oauth

---
+++ web/src/lib/Firebase/config.tsx
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBeJP16hCN2WilgomnqiQhlSnERRIdW0Sk",
+  authDomain: "nlw-return-core.firebaseapp.com",
+  projectId: "nlw-return-core",
+  storageBucket: "nlw-return-core.appspot.com",
+  messagingSenderId: "960813401144",
+  appId: "1:960813401144:web:98f4c89c8700f2c66f52cd",
+};
